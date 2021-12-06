805e0028f8362fabfbdf2d869c3f4189b90c7083
ducdau <dauducdh2cm1@gmail.com>
2021-12-06 13:39:40 +0700

    add read number

--- week-02/Lesson-02/src/BaiTap/DrawShape.java
+++ week-02/Lesson-02/src/BaiTap/DrawShape.java
@@ -7,12 +7,12 @@ public class DrawShape {
 
         Scanner scanner = new Scanner(System.in);
         int choice = 0;
+        int choice2 = 0;
 
-        while (choice != 4) {
+        while (choice != 3) {
             System.out.println("1. Draw the triangle");
-            System.out.println("2. Draw the rectangle 01");
-            System.out.println("3. Draw the rectangle 02");
-            System.out.println("4. Exit");
+            System.out.println("2. Draw the rectangle");
+            System.out.println("3. Exit");
             System.out.println("Enter your choice: ");
             choice = scanner.nextInt();
 
@@ -26,28 +26,68 @@ public class DrawShape {
                     }
                     break;
                 case 2:
-                    for (int i = 0; i < 5; i++) {
-                        for (int j = 0; j <= i; j++) {
-                            System.out.print("* ");
+                    while (choice2 != 5) {
+                        System.out.println("1. Draw the rectangle 01");
+                        System.out.println("2. Draw the rectangle 02");
+                        System.out.println("3. Draw the rectangle 03");
+                        System.out.println("4. Draw the rectangle 04");
+                        System.out.println("5. Exit");
+                        System.out.println("Enter your choice: ");
+                        choice2 = scanner.nextInt();
+
+                        switch (choice2) {
+                            case 1:
+                                for (int i = 0; i < 5; i++) {
+                                    for (int j = 0; j <= i; j++) {
+                                        System.out.print("* ");
+                                    }
+                                    for (int j = 5 - i; j > 0; j--) {
+                                        System.out.print("  ");
+                                    }
+                                    System.out.println(" ");
+                                }
+                                break;
+                            case 2:
+                                for (int i = 0; i < 5; i++) {
+                                    for (int j = 5 - i; j > 0; j--) {
+                                        System.out.print("* ");
+                                    }
+                                    for (int j = 0; j < i; j++) {
+                                        System.out.print("  ");
+                                    }
+                                    System.out.println(" ");
+                                }
+                                break;
+                            case 3:
+                                for (int i = 0; i < 5; i++) {
+                                    for (int j = 4 - i; j > 0; j--) {
+                                        System.out.print("  ");
+                                    }
+                                    for (int j = 0; j <= i; j++) {
+                                        System.out.print("* ");
+                                    }
+                                    System.out.println(" ");
+                                }
+                                break;
+                            case 4:
+                                for (int i = 0; i < 5; i++) {
+                                    for (int j = 0; j < i; j++) {
+                                        System.out.print("  ");
+                                    }
+                                    for (int j = 5 - i; j > 0; j--) {
+                                        System.out.print("* ");
+                                    }
+                                    System.out.println(" ");
+                                }
+                                break;
+
+                            case 5:
+                                break;
                         }
-                        for (int j = 5 - i; j > 0; j--) {
-                            System.out.print("  ");
-                        }
-                        System.out.println(" ");
                     }
+                    choice2 = 0;
                     break;
                 case 3:
-                    for (int i = 0; i < 5; i++) {
-                        for (int j = 5 - i; j > 0; j--) {
-                            System.out.print("* ");
-                        }
-                        for (int j = 0; j < i; j++) {
-                            System.out.print("  ");
-                        }
-                        System.out.println(" ");
-                    }
-                    break;
-                case 4:
                     System.exit(0);
                 default:
                     System.out.println("No choice!!!");
---
+++ week-02/Lesson-02/src/BaiTap/DrawShape2.java
@@ -0,0 +1,102 @@
+package BaiTap;
+
+import java.util.Scanner;
+
+public class DrawShape2 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int choice = 0;
+        int choice2 = 0;
+        while (choice != 3) {
+            System.out.println("1. Draw the triangle");
+            System.out.println("2. Draw the rectangle");
+            System.out.println("3. Exit");
+            System.out.println("Enter your choice: ");
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    for (int i = 0; i < 3; i++) {
+                        for (int j = 0; j < 7; j++) {
+                            System.out.print("* ");
+                        }
+                        System.out.println(" ");
+                    }
+                    break;
+                case 2:
+                    Rectangle(choice2);
+                case 3:
+                    System.exit(0);
+                default:
+                    System.out.println("No choice!!!");
+            }
+
+        }
+    }
+
+    public static String Rectangle(int choiceNumber) {
+        choiceNumber = 0;
+        while (choiceNumber != 5) {
+            Scanner scanner2 = new Scanner(System.in);
+            System.out.println("1. Draw the rectangle 01");
+            System.out.println("2. Draw the rectangle 02");
+            System.out.println("3. Draw the rectangle 03");
+            System.out.println("4. Draw the rectangle 04");
+            System.out.println("5. Exit");
+            System.out.println("Enter your choice: ");
+            choiceNumber = scanner2.nextInt();
+
+            switch (choiceNumber) {
+                case 1:
+                    for (int i = 0; i < 5; i++) {
+                        for (int j = 0; j <= i; j++) {
+                            System.out.print("* ");
+                        }
+                        for (int j = 5 - i; j > 0; j--) {
+                            System.out.print("  ");
+                        }
+                        System.out.println(" ");
+                    }
+                    break;
+                case 2:
+                    for (int i = 0; i < 5; i++) {
+                        for (int j = 5 - i; j > 0; j--) {
+                            System.out.print("* ");
+                        }
+                        for (int j = 0; j < i; j++) {
+                            System.out.print("  ");
+                        }
+                        System.out.println(" ");
+                    }
+                    break;
+                case 3:
+                    for (int i = 0; i < 5; i++) {
+                        for (int j = 4 - i; j > 0; j--) {
+                            System.out.print("  ");
+                        }
+                        for (int j = 0; j <= i; j++) {
+                            System.out.print("* ");
+                        }
+                        System.out.println(" ");
+                    }
+                    break;
+                case 4:
+                    for (int i = 0; i < 5; i++) {
+                        for (int j = 0; j < i; j++) {
+                            System.out.print("  ");
+                        }
+                        for (int j = 5 - i; j > 0; j--) {
+                            System.out.print("* ");
+                        }
+                        System.out.println(" ");
+                    }
+                    break;
+
+                case 5:
+                    break;
+            }
+        }
+        return String.valueOf(choiceNumber);
+    }
+}
