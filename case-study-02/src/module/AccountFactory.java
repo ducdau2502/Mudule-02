e5c69f1b966794d5565a570e8ad73bd74f369040
ducdau <dauducdh2cm1@gmail.com>
2022-01-10 13:56:56 +0700

    refactor cuoi

--- case-study-02/src/module/AccountFactory.java
+++ case-study-02/src/module/AccountFactory.java
@@ -1,6 +1,9 @@
 package module;
 
+import IOfiles.IOFile;
+
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -8,10 +11,14 @@ import java.util.regex.Pattern;
 public class AccountFactory implements Serializable {
     private static Pattern patternId;
     private static Pattern patternPass;
+    private final IOFile<Account> accountIOFile = new IOFile<>();
+    private final ArrayList<Account> accounts;
+    private static final String PATH = "src/database/accounts";
     private static final String REGEX_ID = "^(?!\\d+$)\\w+$";
     private static final String REGEX_PASS = "^(\\w+)$";
 
     public AccountFactory() {
+        accounts = accountIOFile.readFromFile(PATH);
         patternId = Pattern.compile(REGEX_ID);
         patternPass = Pattern.compile(REGEX_PASS);
     }
@@ -26,15 +33,24 @@ public class AccountFactory implements Serializable {
         return !matcher.matches();
     }
 
+    private boolean checkExistId(String string) {
+        for (Account account : accounts) {
+            if (account.getUsername().equals(string)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public Account createAccount(String str) {
         Scanner scanner = new Scanner(System.in);
-        if ("abc".equalsIgnoreCase(str)) {
+        if ("bạn tôi số 1".equalsIgnoreCase(str)) {
             String id;
             String pass;
             do {
                 System.out.print("Nhập tài khoản: ");
                 id = scanner.nextLine();
-            } while (validateId(id));
+            } while (validateId(id) || checkExistId(id));
             do {
                 System.out.print("Nhập nhập khẩu: ");
                 pass = scanner.nextLine();
