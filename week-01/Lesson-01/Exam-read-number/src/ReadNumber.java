193780435012d93f704491207d66edf409ce02a4
Đức Đậu <ducdau@ucs-MacBook-Pro.local>
2022-05-24 14:41:34 +0700

    add

--- week-01/Lesson-01/Exam-read-number/src/ReadNumber.java
+++ week-01/Lesson-01/Exam-read-number/src/ReadNumber.java
@@ -3,125 +3,125 @@ import java.util.Scanner;
 public class ReadNumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Nhập vào 1 số từ 1 - 999 bất kỳ: ");
-        int number = scanner.nextInt();
+        System.out.println("Nhập vào 1 số từ 1 - 9999 bất kỳ: ");
+        String number = scanner.nextLine();
+        System.out.println(ChuyenSo(number));
 
-        int hangDonVi = number % 10;
-        int hangChuc = (number % 100) / 10;
-        int hangTram = number / 100;
 
-        if (number < 0 || number > 999) {
-            System.out.println("Nhập sai giá trị, không đọc được");
-        } else if (number == 0) {
-            System.out.println("zero");
-        } else if (number < 20) {
-            String word = donVi(number);
-            System.out.println(word);
-        } else {
-            String word;
-            if (hangTram == 0) {
-                word = hangChuc(hangChuc) + " " + donVi(hangDonVi);
-            } else if (hangChuc == 1) {
-                word = donVi(hangTram) + " hundred " + donVi(number % 100);
-            } else {
-                word = donVi(hangTram) + " hundred " + hangChuc(hangChuc) + " " + donVi(hangDonVi);
+    }
+    public static String ChuyenSo(String number)
+    {
+        String[] dv = { "", "muoi", "tram", "nghin", "trieu", "ti"};
+        String[] cs = { "khong", "mot", "hai", "ba", "bon", "nam", "sau", "bay", "tam", "chin" };
+        String doc;
+        int i, j, k, n, len, found, ddv, rd;
+
+        len = number.length();
+        number += "ss";
+        doc = "";
+        found=0;
+        ddv=0;
+        rd=0;
+
+        i = 0;
+        while (i < len)
+        {
+            //So chu so o hang dang duyet
+            n = (len - i + 2) % 3 + 1;
+
+            //Kiem tra so 0
+            found = 0;
+            for (j = 0; j < n; j++)
+            {
+                if (number.charAt(i+j) != '0')
+                {
+                    found = 1;
+                    break;
+                }
             }
-            System.out.println(word);
-        }
-    }
 
-    public static String hangChuc(int number) {
-        String hangChucWord = "";
-        switch (number) {
-            case 2:
-                hangChucWord = "twenty";
-                break;
-            case 3:
-                hangChucWord = "thirty";
-                break;
-            case 4:
-                hangChucWord = "fourty";
-                break;
-            case 5:
-                hangChucWord = "fifty";
-                break;
-            case 6:
-                hangChucWord = "sixty";
-                break;
-            case 7:
-                hangChucWord = "seventy";
-                break;
-            case 8:
-                hangChucWord = "eighty";
-                break;
-            case 9:
-                hangChucWord = "ninety";
-                break;
-        }
-        return hangChucWord;
-    }
+            //Duyet n chu so
+            if (found == 1)
+            {
+                rd = 1;
+                for (j = 0; j < n; j++)
+                {
+                    ddv=1;
+                    switch(number.charAt(i+j))
+                    {
+                        case '0':
+                            if (n-j==3) doc+=cs[0]+" ";
+                            if (n-j==2)
+                            {
+                                if (number.charAt(i+j+1)!='0') doc+="linh ";
+                                ddv=0;
+                            }
+                            break;
+                        case '1':
+                            if (n-j==3) doc+=cs[1] + " ";
+                            if (n-j==2)
+                            {
+                                doc+="muoi ";
+                                ddv=0;
+                            }
+                            if (n-j==1)
+                            {
+                                if (i + j == 0) k = 0;
+                                else k = i + j - 1;
 
-    public static String donVi(int number) {
-        String donViWord = "";
-        switch (number) {
-            case 1:
-                donViWord = "one";
-                break;
-            case 2:
-                donViWord = "two";
-                break;
-            case 3:
-                donViWord = "three";
-                break;
-            case 4:
-                donViWord = "four";
-                break;
-            case 5:
-                donViWord = "five";
-                break;
-            case 6:
-                donViWord = "six";
-                break;
-            case 7:
-                donViWord = "seven";
-                break;
-            case 8:
-                donViWord = "eight";
-                break;
-            case 9:
-                donViWord = "nine";
-                break;
-            case 10:
-                donViWord = "ten";
-                break;
-            case 11:
-                donViWord = "eleven";
-                break;
-            case 12:
-                donViWord = "twelve";
-                break;
-            case 13:
-                donViWord = "thirteen";
-                break;
-            case 14:
-                donViWord = "fourteen";
-                break;
-            case 15:
-                donViWord = "fifteen";
-                break;
-            case 16:
-                donViWord = "sixteen";
-                break;
-            case 17:
-                donViWord = "seventeen";
-                break;
-            case 18:
-                donViWord = "eighteen";
-                break;
-            case 19:
-                donViWord = "nineteen";
-                break;
+                                if (number.charAt(k)!='1' && number.charAt(k)!='0')
+                                    doc += "mot ";
+                                else
+                                    doc+=cs[1]+"";
+                            }
+                            break;
+                        case '5':
+                            if (i+j==len-1)
+                                doc+="nam ";
+                            else
+                                doc+=cs[5]+" ";
+                            break;
+                        case '4':
+                            if (i+j==len-1)
+                                doc+="tu ";
+                            else
+                                doc+=cs[4]+"";
+                            break;
+                        default:
+                            doc+=cs[(int)number.charAt(i+j)-48]+" ";
+                            break;
+                    }
+
+                    //Doc don vi nho
+                    if (ddv==1)
+                    {
+                        doc+=dv[n-j-1]+" ";
+                    }
+                }
+            }
+
+
+            //Doc don vi lon
+            if (len-i-n>0)
+            {
+                if ((len - i - n ) % 9 == 0)
+                {
+                    if (rd==1)
+                        for (k = 0; k < (len - i - n) / 9; k++)
+                            doc += "ti ";
+                    rd=0;
+                }
+                else
+                if (found!=0) doc+=dv[((len-i-n+1)%9)/3+2]+" ";
+            }
+
+            i+=n;
         }
-        return donViWord;
+
+        if (len == 1)
+            if (number.charAt(0) == '0' || number.charAt(0) == '5') return cs[(int)number.charAt(0) - 48];
+
+        return doc;
     }
+    
 }
